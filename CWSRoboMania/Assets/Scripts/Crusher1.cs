a1da7f8a2202a09fbe5048e893400804cc872ed8
charlesdoran01 <CamdenSharp123@gmail.com>
2024-04-29 17:34:31 -0400

    STEP 35

---
+++ CWSRoboMania/Assets/Scripts/Crusher1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crusher1 : MonoBehaviour
+{
+	public float speed;
+
+	private void FixedUpdate()
+	{
+		if (transform.position.x <= -8)
+		{
+			speed *= -1
+		}
+		float newXPosition = transform.position.x + speed * Time.fixedDeltaTime;
+		float newYPosition = transform.position.y;
+		Vector2 newPosition = new Vector2(newXPosition, newYPosition);
+		transform.position = newPosition;
+	}
+}
